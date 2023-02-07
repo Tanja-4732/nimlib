fc1010f3c60bf188e376695804905c95021a8ea5
Tanja <git@tanja.pw>
2023-02-07 17:05:38 +0100

    Better documentation for the crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
-//! # NimLib
+//! [![github]](https://github.com/Tanja-4732/nimlib)&ensp;[![crates-io]](https://crates.io/crates/nimlib)&ensp;[![docs-rs]](https://docs.rs/nimlib)
 //!
-//! A Rust library for [Nim games](https://en.wikipedia.org/wiki/Nim): calculate nimbers and possible moves
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+//!
+//! NimLib is a Rust library for [Nim games](https://en.wikipedia.org/wiki/Nim): calculate nimbers and possible moves
 //!
 //! NimLib is work-in-progress at the moment.  
 //! Features such as Poker-Nim (coin pools) are not yet implemented.
