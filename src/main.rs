ecc04415f4b42c85dfc6bbb24dcc2e26057c70be
Tanja <git@tanja.pw>
2023-02-17 20:16:09 +0100

    More refactors & docs for the `MakeRuleSet` command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+//! A CLI for the `nimlib` crate
+//!
+//! See
+//!
+//! ```bash
+//! nimlib --help
+//! nimlib nimber --help
+//! nimlib splits --help
+//! nimlib make-rule-set --help
+//! ```
+
+#![deny(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+
 use clap::{Args, Parser};
 use nimlib::{nimbers, NimRule, Split, Stack, TakeSize};
 
@@ -25,25 +39,34 @@ enum Action {
 
 #[derive(Args)]
 struct MakeRuleSet {
+    /// A list of heights which remainder cannot be split
     #[arg(long, short = 'n')]
     take_split_never: Vec<u64>,
 
+    /// A list of heights which remainder can be split optionally
     #[arg(long, short = 'o')]
     take_split_optional: Vec<u64>,
 
+    /// A list of heights which remainder must be split
     #[arg(long, short = 'a')]
     take_split_always: Vec<u64>,
 
+    /// Allow for taking arbitrary amounts of coins (split(s): never, optional, always)
     #[arg(long, short = 's')]
-    allow_any_take: Vec<Split>,
+    allow_any_take: Option<Split>,
 
+    /// Allow for placing arbitrary amounts of coins (to be implemented)
     #[arg(long, short = 'p')]
     allow_place: bool,
 
+    /// Pretty-print the JSON output
     #[arg(long, short = 'P')]
     pretty_print: bool,
 }
 
+/// The main function of the nimlib CLI
+///
+/// Parses the CLI arguments and handles the subcommands
 pub fn main() {
     let args = Cli::parse();
     match args.action {
@@ -106,27 +129,26 @@ pub fn main() {
                 });
             }
 
-            for split in allow_any_take {
-                match split {
-                    Split::Never => {
-                        rule_set.push(NimRule {
-                            take: TakeSize::Any,
-                            split: Split::Never,
-                        });
-                    }
-                    Split::Optional => {
-                        rule_set.push(NimRule {
-                            take: TakeSize::Any,
-                            split: Split::Optional,
-                        });
-                    }
-                    Split::Always => {
-                        rule_set.push(NimRule {
-                            take: TakeSize::Any,
-                            split: Split::Always,
-                        });
-                    }
+            match allow_any_take {
+                Some(Split::Never) => {
+                    rule_set.push(NimRule {
+                        take: TakeSize::Any,
+                        split: Split::Never,
+                    });
                 }
+                Some(Split::Optional) => {
+                    rule_set.push(NimRule {
+                        take: TakeSize::Any,
+                        split: Split::Optional,
+                    });
+                }
+                Some(Split::Always) => {
+                    rule_set.push(NimRule {
+                        take: TakeSize::Any,
+                        split: Split::Always,
+                    });
+                }
+                None => {}
             }
 
             if allow_place {
